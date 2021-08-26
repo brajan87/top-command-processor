852ce2d8845222ff95713451a09e3a2cf8cddcab
BalaPrakash Rajan (OSV) <balaprakash.rajan.osv@fedex.com>
2021-08-26 11:02:38 -0400

    Removed unwanted file

--- src/main/java/com/ground/fedex/mockresponseserver/model/RegisterEndpointRequest.java
+++
@@ -1,9 +0,0 @@
-package com.ground.fedex.mockresponseserver.model;
-
-import lombok.Data;
-
-@Data
-public class RegisterEndpointRequest {
-  private String url;
-  private String expectedPayload;
-}
